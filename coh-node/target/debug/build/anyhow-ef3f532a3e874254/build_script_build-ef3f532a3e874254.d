34eac9b611624ec4dac0660f14871e67c7e6bbeb
True A <truea@noetic.ai>
2026-04-09 15:55:58 -0500

    chore: ignore Rust build artifacts in target directory

---
+++ coh-node/target/debug/build/anyhow-ef3f532a3e874254/build_script_build-ef3f532a3e874254.d
@@ -0,0 +1,5 @@
+C:\Users\truea\.gemini\antigravity\brain\75ab5e4c-a69b-451c-b975-f05bf7a3baf3\Coh wedge\coh-node\target\debug\build\anyhow-ef3f532a3e874254\build_script_build-ef3f532a3e874254.d: C:\Users\truea\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\anyhow-1.0.102\build.rs
+
+C:\Users\truea\.gemini\antigravity\brain\75ab5e4c-a69b-451c-b975-f05bf7a3baf3\Coh wedge\coh-node\target\debug\build\anyhow-ef3f532a3e874254\build_script_build-ef3f532a3e874254.exe: C:\Users\truea\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\anyhow-1.0.102\build.rs
+
+C:\Users\truea\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\anyhow-1.0.102\build.rs:
---
+++ coh-node/target/debug/build/generic-array-90aaaf90cf2e7517/build_script_build-90aaaf90cf2e7517.d
@@ -0,0 +1,5 @@
+C:\Users\truea\.gemini\antigravity\brain\75ab5e4c-a69b-451c-b975-f05bf7a3baf3\Coh wedge\coh-node\target\debug\build\generic-array-90aaaf90cf2e7517\build_script_build-90aaaf90cf2e7517.d: C:\Users\truea\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\generic-array-0.14.7\build.rs
+
+C:\Users\truea\.gemini\antigravity\brain\75ab5e4c-a69b-451c-b975-f05bf7a3baf3\Coh wedge\coh-node\target\debug\build\generic-array-90aaaf90cf2e7517\build_script_build-90aaaf90cf2e7517.exe: C:\Users\truea\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\generic-array-0.14.7\build.rs
+
+C:\Users\truea\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\generic-array-0.14.7\build.rs:
